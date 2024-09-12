7fbc6410af00d820b61e4afc9d958a8617a449c6
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-09-12 00:35:44 -0500

    update-frontend

--- frontclient/src/pages/loginPage.jsx
+++ frontclient/src/pages/loginPage.jsx
@@ -24,29 +24,31 @@ export function FormularioSesion() {
   })
 
   return (
-    <div className="container-u">
-      {
-        signinErrors.map((error, i) => (
-          <div className="bg-red-500 p-2 text-white my-2" key={i}>
-            {error}
-          </div>
-        ))
-      }
-      <form onSubmit={onSubmit} className="form-register">
-        <label
-          className="form-label"><input type="email" {...register('email', { required: true })} placeholder="Email" className="info-childs-input" />
-        </label>
-        {errors.email && (<p className="text-red-500"> Email es Necesario! </p>)}
-        <label
-          className="form-label"><input type="password" {...register('password', { required: true })} placeholder="Password" className="info-childs-input" />
-        </label>
-        {errors.password && (<p className="text-red-500"> Password es Necesario! </p>)}
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+        {
+          signinErrors.map((error, i) => (
+            <div className="bg-red-500 p-2 text-white my-2" key={i}>
+              {error}
+            </div>
+          ))
+        }
+        <form onSubmit={onSubmit} className="form-register">
+          <label
+            className="form-label"><input type="email" {...register('email', { required: true })} placeholder="Email" className="info-childs-input" />
+          </label>
+          {errors.email && (<p className="text-red-500"> Email es Necesario! </p>)}
+          <label
+            className="form-label"><input type="password" {...register('password', { required: true })} placeholder="Password" className="info-childs-input" />
+          </label>
+          {errors.password && (<p className="text-red-500"> Password es Necesario! </p>)}
 
-        <button type="submit" value="container4-button1" className="registerbtn">Iniciar Sesion</button>
-      </form>
-      <p className="flex gap-x-2 justify-between">
-        No tienes una cuenta? <Link href="/register" className="text-sky-0">Registrarse</Link>
-      </p>
+          <button type="submit" value="container4-button1" className="registerbtn">Iniciar Sesion</button>
+        </form>
+        <p className="flex gap-x-2 justify-between">
+          No tienes una cuenta? <Link href="/register" className="text-sky-0">Registrarse</Link>
+        </p>
+      </div>
     </div>
   );
 }
