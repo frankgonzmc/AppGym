40f95e7ce82d0e59f364e6204f8199cce1e75c5e
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-09-20 23:45:34 -0500

    update-app.jsx

--- frontclient/src/App.jsx
+++ frontclient/src/App.jsx
@@ -13,7 +13,6 @@ import RutinaForm from './pages/rutinaForm';
 import Navbar from './components/navbar';
 import ProgresoPage from './pages/progresoPage';
 import HistorialPage from './pages/historialPage';
-
 import EjercicioForm from './pages/ejercicioForm';
 import EjercicioPage from './pages/ejercicioPage';
 import { ProgresoProvider } from './context/progreso';
@@ -21,39 +20,34 @@ import { ProgresoProvider } from './context/progreso';
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/register' element={<RegistroUsuario />} />
-
-          <Route element={<ProtectedRoute />}>
-            <Route path='/' element={<Inicio />} />
-            <Route path='/profile' element={<ProfilePage />} />
-            <Route path='/inicio' element={<Inicio />} />
-
-            <RutinaProvider>
-              <Route path='/rutinas' element={<RutinaPage />} />
-              <Route path='/add-rutinas' element={<RutinaForm />} />
-              <Route path='/rutina/:id' element={<RutinaForm />} />
-            </RutinaProvider>
-
-            <HistorialProvider>
-              <Route path='/historial/:id' element={<HistorialPage />} />
-            </HistorialProvider>
-
-            <ProgresoProvider>
-              <Route path='/progresos/:id' element={<ProgresoPage />} />
-            </ProgresoProvider>
-
+      <RutinaProvider>
+        <HistorialProvider>
+          <ProgresoProvider>
             <EjercicioProvider>
-              <Route path='/ejercicios' element={<EjercicioPage />} />
-              <Route path='/add-ejercicios' element={<EjercicioForm />} />
-              <Route path='/ejercicio/:id' element={<EjercicioForm />} />
+              <BrowserRouter>
+                <Navbar />
+                <Routes>
+                  <Route path='/login' element={<LoginPage />} />
+                  <Route path='/register' element={<RegistroUsuario />} />
+                  <Route element={<ProtectedRoute />}>
+                    <Route path='/' element={<Inicio />} />
+                    <Route path='/profile' element={<ProfilePage />} />
+                    <Route path='/inicio' element={<Inicio />} />
+                    <Route path='/rutinas' element={<RutinaPage />} />
+                    <Route path='/add-rutinas' element={<RutinaForm />} />
+                    <Route path='/rutina/:id' element={<RutinaForm />} />
+                    <Route path='/historial/:id' element={<HistorialPage />} />
+                    <Route path='/progresos/:id' element={<ProgresoPage />} />
+                    <Route path='/ejercicios' element={<EjercicioPage />} />
+                    <Route path='/add-ejercicios' element={<EjercicioForm />} />
+                    <Route path='/ejercicio/:id' element={<EjercicioForm />} />
+                  </Route>
+                </Routes>
+              </BrowserRouter>
             </EjercicioProvider>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+          </ProgresoProvider>
+        </HistorialProvider>
+      </RutinaProvider>
     </AuthProvider>
   );
 }
