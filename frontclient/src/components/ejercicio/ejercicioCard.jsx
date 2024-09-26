3662d18de203ed686487cc3c73c160419d1358eb
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-09-26 01:56:00 -0500

    update-front-angeles

--- frontclient/src/components/ejercicio/ejercicioCard.jsx
+++ frontclient/src/components/ejercicio/ejercicioCard.jsx
@@ -1,7 +1,8 @@
 import { useEjercicios } from "../../context/ejercicioscontext";
 import { Button, ButtonLink, Card } from "../ui";
 
-export function EjercicioCard({ detalle }) {
+
+export function EjercicioCard({ ejercicio }) {
   const { deleteEjercicio } = useEjercicios();
 
   return (
@@ -9,17 +10,25 @@ export function EjercicioCard({ detalle }) {
       <header className="flex justify-between">
         <h1 className="text-2xl font-bold text-center">EJERCICIO</h1>
         <div className="flex gap-x-2 items-center">
-          <Button onClick={() => deleteEjercicio(detalle.ejercicio._id)}>Delete</Button>
-          <ButtonLink to={`/ejercicio/${detalle.ejercicio._id}`}>Edit</ButtonLink>
+          <Button onClick={() => deleteEjercicio(ejercicio._id)}>Delete</Button>
+          <ButtonLink to={`/ejercicio/${ejercicio._id}`}>Edit</ButtonLink>
         </div>
       </header>
-      <p className="text-slate-300 font-semibold">{detalle.ejercicio.nombre}</p>
-      <p className="text-slate-300">{detalle.ejercicio.descripcion}</p>
-      <p className="text-slate-300">Categoría: {detalle.ejercicio.categoria}</p>
-      <p className="text-slate-300">Duración: {detalle.duracion} segundos</p>
-      <p className="text-slate-300">Series: {detalle.series}</p>
-      <p className="text-slate-300">Repeticiones: {detalle.repeticiones}</p>
-      <p className="text-slate-300">Estado: {detalle.estado || 'En Progreso'}</p> {/* Muestra el estado */}
+      <p className="text-slate-700">{ejercicio.nombre}</p>
+      <p className="text-slate-700">{ejercicio.descripcion}</p>
+      <p className="text-slate-700">{ejercicio.categoria}</p>
+      <p className="text-slate-700">{ejercicio.duracion}</p>
+      <p className="text-slate-700">{ejercicio.estado}</p>
+      {/* format date */}
+      <p>
+        {ejercicio.date &&
+          new Date(ejercicio.date).toLocaleDateString("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+      </p>
     </Card>
   );
 }
