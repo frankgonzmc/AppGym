834efed6858865d93edb5362bf5e38880496338b
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-09-28 22:39:22 -0500

    update

--- frontclient/src/components/detallerutina/detallerutinaCard.jsx
+++ frontclient/src/components/detallerutina/detallerutinaCard.jsx
@@ -7,16 +7,14 @@ export default function DetalleRutinaCard({ detalles }) {
   return (
     <Card>
       <header className="flex justify-between">
-        <h1 className="text-2xl font-bold text-center">EJERCICIO</h1>
+        <h1 className="text-2xl font-bold text-center">{detalles.ejercicio.nombre}</h1>
         <div className="flex gap-x-2 items-center">
           <Button onClick={() => deleteDetalleRutina(detalles.ejercicio._id)}>Delete</Button>
-          <ButtonLink to={`/ejercicio/${detalles.ejercicio._id}`}>Edit</ButtonLink>
         </div>
       </header>
-      <p className="text-slate-300 font-semibold">{detalles.ejercicio.nombre}</p>
       <p className="text-slate-300">{detalles.ejercicio.descripcion}</p>
       <p className="text-slate-300">Categoría: {detalles.ejercicio.categoria}</p>
-      <p className="text-slate-300">Duración: {detalles.duracion} segundos</p>
+      <p className="text-slate-300">Duración: {detalles.duracion} minutos</p>
       <p className="text-slate-300">Series: {detalles.series}</p>
       <p className="text-slate-300">Repeticiones: {detalles.repeticiones}</p>
       <p className="text-slate-300">Orden: {detalles.orden}</p>
