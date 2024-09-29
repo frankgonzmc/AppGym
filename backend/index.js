3480d021f8c46a78f951d12e761baa8313244811
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-09-28 23:06:37 -0500

    update-backend

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,16 @@
 import app from "./app.js";
 import { PORT } from "./config.js";
 import { connectDB } from "./db.js";
-import seedEjercicios from './seeds/ejercicioseeds.js'; // Ruta a tu archivo seed.js
+import seedEjercicios from "./seeds/ejercicioseeds.js"; // Ruta a tu archivo seed.js
 
 async function main() {
     try {
-        // Conectar a la base de datos
         await connectDB();
-
-        // Iniciar el servidor
-        app.listen(PORT, () => {
-            console.log(`Conectado en el puerto: ${PORT}`);
-            console.log(`Environment: ${process.env.NODE_ENV}`);
-        });
-
-        // Ejecutar el seeder solo si está en modo desarrollo
-        if (process.env.NODE_ENV === 'development') {
-            console.log("Ejecutando seeders en entorno de desarrollo...");
-            await seedEjercicios();
-            console.log("Seeders ejecutados correctamente.");
-        }
+        app.listen(PORT);
+        seedEjercicios();
+        console.log(`Conectado en el puerto: ${PORT}`);
     } catch (error) {
-        console.error("Error al iniciar la aplicación:", error);
+        console.error(error);
     }
 }
 
