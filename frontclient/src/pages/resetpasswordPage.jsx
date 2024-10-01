3efd49f9bd01d36ac0fca87063696cb701245722
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-10-01 09:48:12 -0500

    update

--- frontclient/src/pages/resetpasswordPage.jsx
+++ frontclient/src/pages/resetpasswordPage.jsx
@@ -1,35 +1,41 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import axios from '../api/axios'
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const [loading, setLoading] = useState(false);
+    const { register, handleSubmit, formState: { errors }, } = useForm();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setLoading(true); // Mostrar carga
-
+    const onSubmit = handleSubmit(async (data) => {
+        setLoading(true);
+        console.log(data);
         try {
-            const response = await axios.post('/forgot-password', { email });
+            const response = await axios.post('/forgot-password', { email: data.email });
             setMessage(response.data.message);
             setEmail(''); // Limpiar el campo de email
         } catch (error) {
             setMessage(error.response ? error.response.data.message : "Error en la solicitud");
+        } finally {
+            setLoading(false); // Ocultar carga
         }
-    };
+    });
+
 
     return (
         <div className="flex h-screen items-center justify-center">
-            <form onSubmit={handleSubmit} className="bg-zinc-800 p-10 rounded-md">
+            <form onSubmit={onSubmit} className="bg-zinc-800 p-10 rounded-md">
                 <h2 className="text-white mb-4">Recuperar Contraseña</h2>
                 <input
                     type="email"
-                    value={email}
+                    {...register('email', { required: true })}
                     placeholder="Ingresa tu correo"
                     required
                     className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md mb-2"
                 />
+                {errors.email && (<p className="text-red-500"> Email es Necesario! </p>)}
+
                 <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md">
                     {loading ? "Enviando..." : "Enviar enlace de recuperación"}
                 </button>
