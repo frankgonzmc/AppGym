5804e8f59eea2d6db0986dd53099a08dba01a7c5
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-10-04 10:49:55 -0500

    update

--- frontclient/src/components/detallerutina/detallerutinaCard.jsx
+++ frontclient/src/components/detallerutina/detallerutinaCard.jsx
@@ -1,44 +1,31 @@
-import { Card } from "../ui";
-import { useNavigate } from 'react-router-dom';
-import { ProgressBar } from 'react-bootstrap'; // Importamos el ProgressBar de react-bootstrap
 import { useDetallesRutina } from "../../context/detallerutinacontext";
+import { Card } from "../ui";
 
-export default function DetallerutinaPage({ rutina }) {
+export default function DetalleRutinaCard({ detalles }) {
   const { deleteDetalleRutina } = useDetallesRutina();
-  const navigate = useNavigate();
-
-  // Calcular progreso basado en ejercicios completados
-  const ejerciciosCompletados = rutina.ejercicios.filter(e => e.estado === 'Completado').length;
-  const totalEjercicios = rutina.ejercicios.length;
-  const progreso = (ejerciciosCompletados / totalEjercicios) * 100;
 
   return (
     <Card>
       <header className="flex justify-between">
-        <h1 className="text-2xl text-slate-300 font-bold text-center">{rutina.nombre}</h1>
+        <h1 className="text-2xl text-slate-300 font-bold text-center">{detalles.ejercicio.nombre}</h1>
       </header>
       <hr className="text-slate-300" />
-      <p className="text-slate-300">Descripción: {rutina.descripcion}</p>
-
-      {/* Formatear la fecha */}
+      <p className="text-slate-300">Descripción: {detalles.ejercicio.descripcion}</p>
+      <p className="text-slate-300">Categoría: {detalles.ejercicio.categoria}</p>
+      <p className="text-slate-300">Duración: {detalles.ejercicio.duracion} segundos</p>
+      <p className="text-slate-300">Series: {detalles.ejercicio.series}</p>
+      <p className="text-slate-300">Repeticiones: {detalles.ejercicio.repeticiones}</p>
+      <p className="text-slate-300">Descanso: {detalles.ejercicio.descanso}</p>
       <p className="text-slate-300">
-        {rutina.date &&
-          new Date(rutina.date).toLocaleDateString("en-US", {
+        {detalles.createdAt &&
+          new Date(detalles.createdAt).toLocaleDateString("en-US", {
             weekday: "long",
             year: "numeric",
             month: "long",
             day: "numeric",
           })}
       </p>
-
       <hr className="text-slate-300" />
-
-      {/* Barra de progreso */}
-      <div className="my-3">
-        <p className="text-slate-300">Progreso de la rutina:</p>
-        <ProgressBar now={progreso} label={`${Math.round(progreso)}%`} />
-        {progreso === 100 && <p className="text-green-500 mt-2">¡Rutina Completada!</p>}
-      </div>
       <footer>
         <div className="flex gap-x-2 items-center">
           <button className="btn btn-primary" onClick={() => deleteDetalleRutina(detalles.ejercicio._id)}>Delete</button>
