64b4bc6d3613be59c508b27bc9584c22e57202b1
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-10-08 21:11:12 -0500

    update-frontend

--- frontclient/src/pages/auth/profilePage.jsx
+++ frontclient/src/pages/auth/profilePage.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import profileImage from "../../imagenes/profileicono.png";
 
 function ProfilePage() {
-  const { user, updatePassword, updatePerfil, checkEmailExists } = useAuth();
+  const { user, updatePassword, updatePerfil } = useAuth();
   const [password, setPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -45,20 +45,42 @@ function ProfilePage() {
   const handleUpdateProfile = async (e) => {
     e.preventDefault();
 
+    // Validar que los campos no estén vacíos
     if (!nombreCompleto || !edad || !estatura || !peso || !nuevoEmail) {
       setError("Todos los campos deben estar completos.");
       return;
     }
 
-    // Verificar si el nuevo email ya existe
-    const emailExists = await checkEmailExists(nuevoEmail);
-    if (emailExists) {
-      setError("El email no está disponible. Por favor, elige otro.");
+    // Verificar si hay cambios en el perfil
+    const isEmailChanged = nuevoEmail !== user.email;
+    const isProfileChanged =
+      nombreCompleto !== user.username ||
+      edad !== user.edad ||
+      estatura !== user.estatura ||
+      peso !== user.peso;
+
+    if (!isEmailChanged && !isProfileChanged) {
+      setError("No hay cambios para actualizar.");
       return;
     }
 
+    // Si el email ha cambiado, verificar si ya existe
+    if (isEmailChanged) {
+      const emailExists = await checkEmailExists(nuevoEmail);
+      if (emailExists) {
+        setError("El email no está disponible. Por favor, elige otro.");
+        return;
+      }
+    }
+
     try {
-      await updatePerfil({ username: nombreCompleto, edad, estatura, peso, email: nuevoEmail });
+      await updatePerfil({
+        username: nombreCompleto,
+        edad,
+        estatura,
+        peso,
+        email: nuevoEmail,
+      });
       setSuccess("Perfil actualizado con éxito");
     } catch (error) {
       setError("Error al actualizar el perfil");
@@ -70,7 +92,7 @@ function ProfilePage() {
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        setProfileImg(reader.result); // Actualiza la imagen de perfil
+        setProfileImg(reader.result);
       };
       reader.readAsDataURL(file);
     }
@@ -80,78 +102,79 @@ function ProfilePage() {
     <div className="container mx-auto p-6">
       {/* Sección de Perfil */}
       <section className="bg-zinc-800 p-6 rounded-md shadow-lg flex items-center justify-between mb-8">
-        <section>
-          <div className="flex-1 text-white">
-            <h2 className="text-3xl font-bold mb-4">Perfil de Usuario</h2>
-            <form onSubmit={handleUpdateProfile}>
-              <div className="space-y-4">
+        <div className="flex-1 text-white">
+          <h2 className="text-3xl font-bold mb-4">Perfil de Usuario</h2>
+          <form onSubmit={handleUpdateProfile}>
+            <div className="space-y-4">
+              <div>
+                <label className="block text-gray-400">Nombre completo:</label>
+                <input
+                  type="text"
+                  value={nombreCompleto}
+                  onChange={(e) => setNombreCompleto(e.target.value)}
+                  className="w-full p-2 border border-gray-400 rounded-md text-black"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-400">Nuevo Email:</label>
+                <input
+                  type="email"
+                  value={nuevoEmail}
+                  onChange={(e) => setNuevoEmail(e.target.value)}
+                  className="w-full p-2 border border-gray-400 rounded-md text-black"
+                />
+              </div>
+              <div className="flex space-x-4">
                 <div>
-                  <label className="block text-gray-400">Nombre completo:</label>
+                  <label className="block text-gray-400">Edad:</label>
                   <input
-                    type="text"
-                    value={nombreCompleto}
-                    onChange={(e) => setNombreCompleto(e.target.value)}
-                    className="w-full p-2 border border-gray-400 rounded-md text-black"
+                    type="number"
+                    value={edad}
+                    onChange={(e) => setEdad(e.target.value)}
+                    className="p-2 border border-gray-400 rounded-md text-black"
                   />
                 </div>
                 <div>
-                  <label className="block text-gray-400">Nuevo Email:</label>
+                  <label className="block text-gray-400">Estatura:</label>
                   <input
-                    type="email"
-                    value={nuevoEmail}
-                    onChange={(e) => setNuevoEmail(e.target.value)}
-                    className="w-full p-2 border border-gray-400 rounded-md text-black"
+                    type="number"
+                    value={estatura}
+                    onChange={(e) => setEstatura(e.target.value)}
+                    className="p-2 border border-gray-400 rounded-md text-black"
                   />
                 </div>
-                <div className="flex space-x-4">
-                  <div>
-                    <label className="block text-gray-400">Edad:</label>
-                    <input
-                      type="number"
-                      value={edad}
-                      onChange={(e) => setEdad(e.target.value)}
-                      className="p-2 border border-gray-400 rounded-md text-black"
-                    />
-                  </div>
-                  <div>
-                    <label className="block text-gray-400">Estatura:</label>
-                    <input
-                      type="number"
-                      value={estatura}
-                      onChange={(e) => setEstatura(e.target.value)}
-                      className="p-2 border border-gray-400 rounded-md text-black"
-                    />
-                  </div>
-                  <div>
-                    <label className="block text-gray-400">Peso:</label>
-                    <input
-                      type="number"
-                      value={peso}
-                      onChange={(e) => setPeso(e.target.value)}
-                      className="p-2 border border-gray-400 rounded-md text-black"
-                    />
-                  </div>
+                <div>
+                  <label className="block text-gray-400">Peso:</label>
+                  <input
+                    type="number"
+                    value={peso}
+                    onChange={(e) => setPeso(e.target.value)}
+                    className="p-2 border border-gray-400 rounded-md text-black"
+                  />
                 </div>
               </div>
+            </div>
 
-              {error && <p className="text-red-500">{error}</p>}
-              {success && <p className="text-green-500">{success}</p>}
+            {error && <p className="text-red-500">{error}</p>}
+            {success && <p className="text-green-500">{success}</p>}
 
-              <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4">Actualizar Perfil</button>
-            </form>
-          </div>
-        </section>
-        <section>
-          {/* Imagen de perfil */}
-          <div className="w-32 h-32">
-            <img
-              src={profileImg}
-              alt="Profile"
-              className="w-full h-full object-cover rounded-full"
-            />
-            <input type="file" onChange={handleImageUpload} className="mt-2" />
-          </div>
-        </section>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+            >
+              Actualizar Perfil
+            </button>
+          </form>
+        </div>
+        {/* Imagen de perfil */}
+        <div className="w-32 h-32">
+          <img
+            src={profileImg}
+            alt="Profile"
+            className="w-full h-full object-cover rounded-full"
+          />
+          <input type="file" onChange={handleImageUpload} className="mt-2" />
+        </div>
       </section>
 
       {/* Sección de Actualización de Contraseña */}
