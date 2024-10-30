08a7b8b30d391ef1e68d50e6ac73114445ccf6f8
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-10-30 12:55:36 -0500

    update

--- frontclient/src/components/ejercicio/ejercicioCard.jsx
+++ frontclient/src/components/ejercicio/ejercicioCard.jsx
@@ -1,4 +1,5 @@
 import { useEjercicios } from "../../context/ejercicioscontext";
+import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from "../../context/authcontext";
 import { Button, ButtonLink, Card } from "../ui";
