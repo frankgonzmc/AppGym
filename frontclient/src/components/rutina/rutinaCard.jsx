eda61d07b5f97478ae0258594fae6b424de4c998
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-10-30 14:09:38 -0500

    update

--- frontclient/src/components/rutina/rutinaCard.jsx
+++ frontclient/src/components/rutina/rutinaCard.jsx
@@ -29,20 +29,9 @@ export function RutinaCard({ rutina }) {
   useEffect(() => {
     const fetchDetalles = async () => {
       try {
-        const detalles = await getDetallesRutina(rutina._id); // Usa getDetallesRutina aquí
-
-        // Verifica que detalles sea un array
-        if (!Array.isArray(detalles)) {
-          console.error("La respuesta de detalles no es un array:", detalles);
-          return; // Salir si no es un array
-        }
-
-        // Filtrar los detalles de rutina que estén completos (4/4)
-        const ejerciciosCompletos = detalles.filter(detalle =>
-          detalle.seriesProgreso >= 4 && detalle.estado === 'Completado'
-        );
-
-        setEjerciciosCompletados(ejerciciosCompletos.length);
+        const detalles = await getDetallesRutina(rutina._id); // Obtén los detalles de la rutina
+        const completados = detalles.filter(detalle => detalle.seriesProgreso === 4).length; // Cuenta los ejercicios completados
+        setEjerciciosCompletados(completados); // Actualiza el estado con la cantidad de ejercicios completados
       } catch (error) {
         console.error("Error al obtener detalles de la rutina:", error);
       }
