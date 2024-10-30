2bc9294a10fc67dba27835ed14b43be23a70b615
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-10-30 13:43:27 -0500

    update

--- frontclient/src/pages/rutinas/rutinaPage.jsx
+++ frontclient/src/pages/rutinas/rutinaPage.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useRutinas } from "../../context/rutinascontext";
+import { useProgreso } from "../../context/progresocontext"; // Importar el contexto de progreso
 import { RutinaCard } from "../../components/rutina/rutinaCard";
 import { ImFileEmpty } from "react-icons/im";
 import { Link } from "react-router-dom";
 
 export default function RutinaPage() {
   const { rutinas, getRutinas } = useRutinas();
+  const { getProgreso } = useProgreso(); // Obtener la función getProgreso del contexto
 
   useEffect(() => {
     const fetchRutinas = async () => {
@@ -14,12 +16,13 @@ export default function RutinaPage() {
         getProgreso(rutina._id); // Obtener progreso para cada rutina
       });
     };
+
     fetchRutinas();
-  }, []);
+  }, [getRutinas, rutinas, getProgreso]); // Agregar dependencias al efecto
 
   return (
     <>
-      {!rutinas || rutinas.length === 0 && (
+      {!rutinas || rutinas.length === 0 ? ( // Cambiar && por ?
         <div className="flex justify-center items-center p-10">
           <div>
             <ImFileEmpty className="text-6xl text-gray-400 m-auto my-2" />
@@ -31,13 +34,13 @@ export default function RutinaPage() {
             </p>
           </div>
         </div>
+      ) : (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-2">
+          {rutinas.map((rutina) => (
+            <RutinaCard rutina={rutina} key={rutina._id} />
+          ))}
+        </div>
       )}
-
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-2">
-        {rutinas && rutinas.map((rutina) => (
-          <RutinaCard rutina={rutina} key={rutina._id} />
-        ))}
-      </div>
     </>
   );
 }
