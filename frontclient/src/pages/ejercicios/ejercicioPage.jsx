d4afdab131cb2874406011c22ff75c30bd851e09
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-10-31 11:16:59 -0500

    update

--- frontclient/src/pages/ejercicios/ejercicioPage.jsx
+++ frontclient/src/pages/ejercicios/ejercicioPage.jsx
@@ -3,6 +3,7 @@ import { useEjercicios } from "../../context/ejercicioscontext";
 import { EjercicioCard } from "../../components/ejercicio/ejercicioCard";
 import { ImFileEmpty } from "react-icons/im";
 import { Link } from "react-router-dom";
+import "../../css/ejercicioPage.css";
 import { Container, Row, Col, Card } from 'react-bootstrap';
 
 export default function EjercicioPage() {
@@ -13,29 +14,31 @@ export default function EjercicioPage() {
   }, []);
 
   return (
-    <Container className="py-4 mt-4">
-      {!ejercicios || ejercicios.length === 0 ? (
-        <Row className="justify-content-center">
-          <Col md={6} className="text-center">
-            <Card className="p-4">
-              <ImFileEmpty className="text-6xl text-gray-400 mb-3" />
-              <h1 className="font-bold">No hay ejercicios por el momento</h1>
-              <p>Agrega uno nuevo para comenzar.</p>
-              <Link to="/add-ejercicios" className="btn btn-primary">
-                CREAR EJERCICIOS
-              </Link>
-            </Card>
-          </Col>
-        </Row>
-      ) : (
-        <Row className="g-4">
-          {ejercicios.map((ejercicio) => (
-            <Col md={6} lg={4} key={ejercicio._id}>
-              <EjercicioCard ejercicio={ejercicio} />
+    <section className="seccion">
+      <Container className="py-4 mt-4">
+        {!ejercicios || ejercicios.length === 0 ? (
+          <Row className="justify-content-center">
+            <Col md={6} className="text-center">
+              <Card className="p-4">
+                <ImFileEmpty className="text-6xl text-gray-400 mb-3" />
+                <h1 className="font-bold">No hay ejercicios por el momento</h1>
+                <p>Agrega uno nuevo para comenzar.</p>
+                <Link to="/add-ejercicios" className="btn btn-primary">
+                  CREAR EJERCICIOS
+                </Link>
+              </Card>
             </Col>
-          ))}
-        </Row>
-      )}
-    </Container>
+          </Row>
+        ) : (
+          <Row className="g-4">
+            {ejercicios.map((ejercicio) => (
+              <Col md={6} lg={4} key={ejercicio._id}>
+                <EjercicioCard ejercicio={ejercicio} />
+              </Col>
+            ))}
+          </Row>
+        )}
+      </Container>
+    </section>
   );
 }
