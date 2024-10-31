d2c673cbd73382687ae6e71f36e1a37e7b31b760
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-10-31 01:06:41 -0500

    update-frotnend

--- frontclient/src/components/ui/Card.jsx
+++ frontclient/src/components/ui/Card.jsx
@@ -1,3 +1,7 @@
 export function Card({ children }) {
-  return <div className="bg-zinc-800 max-w-md w-full p-3 rounded-md">{children}</div>;
+  return (
+    <div className="bg-zinc-800 max-w-md w-full p-4 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+      {children}
+    </div>
+  );
 }
