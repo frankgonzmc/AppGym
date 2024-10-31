45a3ee44e8067e230a5ff234965136e654b0b59d
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-10-31 01:09:15 -0500

    update-frontend

--- frontclient/src/pages/rutinas/rutinaPage.jsx
+++ frontclient/src/pages/rutinas/rutinaPage.jsx
@@ -4,6 +4,7 @@ import { useProgreso } from "../../context/progresocontext"; // Importar el cont
 import { RutinaCard } from "../../components/rutina/rutinaCard";
 import { ImFileEmpty } from "react-icons/im";
 import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
 export default function RutinaPage() {
   const { rutinas, getRutinas } = useRutinas();
@@ -23,23 +24,29 @@ export default function RutinaPage() {
   return (
     <>
       {!rutinas || rutinas.length === 0 ? ( // Cambiar && por ?
-        <div className="flex justify-center items-center p-10">
-          <div>
-            <ImFileEmpty className="text-6xl text-gray-400 m-auto my-2" />
-            <h1 className="font-bold text-xl">
-              No hay Rutinas por el momento, agrega una...
-            </h1>
-            <p className="flex gap-x-2 justify-between">
-              <Link to="/add-rutinas"> CREAR RUTINA</Link>
-            </p>
+        <Card>
+          <div className="flex justify-center items-center p-10">
+            <div>
+              <ImFileEmpty className="text-6xl text-gray-400 m-auto my-2" />
+              <h1 className="font-bold text-xl">
+                No hay Rutinas por el momento, agrega una...
+              </h1>
+              <p className="flex gap-x-2 justify-between">
+                <Link to="/add-rutinas"> CREAR RUTINA</Link>
+              </p>
+            </div>
           </div>
-        </div>
+        </Card>
       ) : (
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-2">
-          {rutinas.map((rutina) => (
-            <RutinaCard rutina={rutina} key={rutina._id} />
-          ))}
-        </div>
+        <section>
+          <Card>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-2">
+              {rutinas.map((rutina) => (
+                <RutinaCard rutina={rutina} key={rutina._id} />
+              ))}
+            </div>
+          </Card>
+        </section>
       )}
     </>
   );
