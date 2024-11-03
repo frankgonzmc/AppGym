a903141c1a94c078de91408f5e3997db670c5ea9
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-03 13:15:13 -0500

    update

--- backend/app.js
+++ backend/app.js
@@ -12,15 +12,9 @@ import { FRONTEND_URL } from "./config.js";
 import { fileURLToPath } from 'url';  // Importar esta utilidadimport dotenv from 'dotenv';
 import dotenv from 'dotenv';
 
-// Crear __dirname manualmente
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 dotenv.config();
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'uploads'));
 
 app.use(cors({
     origin: FRONTEND_URL,
@@ -29,6 +23,9 @@ app.use(cors({
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
+app.use(multer({
+    dest: './uploads/perfil'
+}).single('profileImage'));
 
 app.use("/api", authRoutes);
 app.use("/api", rutinaRoutes);
@@ -36,8 +33,7 @@ app.use("/api", ejercicioRoutes);
 app.use("/api", progresoRoutes);
 app.use("/api", detallerutinaRoutes);
 
-// Configurar ruta estática para acceder a las imágenes subidas
-app.use('/uploads/perfil', express.static(path.join(__dirname, 'uploads/perfil')));
+
 app.use('/uploads/ejercicios', express.static(path.join(__dirname, 'uploads/ejercicios')));
 
 export default app;
