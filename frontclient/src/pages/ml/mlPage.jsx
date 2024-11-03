2d016393252742a145b0caf63af67ea5e5f46e5b
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-02 22:29:17 -0500

    update

--- frontclient/src/pages/ml/mlPage.jsx
+++ frontclient/src/pages/ml/mlPage.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-
-
+import { useAuth } from "../context/authcontext";
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 export default function mlPage() {
 
-    
+    const { user } = useAuth();
 
     return (
-        <div>mlPage</div>
+        <Container>
+            <Row>
+                <Col className="text-center">
+                    <Card className="info-card mt-3">
+                        <Card.Body>
+                            <Card.Title>¿recomendaciones de rutinas para ejercicios?</Card.Title>
+                            <p>
+                                ¡No te pierdas la oportunidad de mejorar tus habilidades!
+                                ¡Aprende a mejorar tus habilidades y mejorar tu vida diaria!
+                            </p>
+
+                            <p> esta es tu predicción en base a tus objetivos seleccionados  para tus rutinas de ejercicios y rutina de alimentación: </p>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     )
 }
