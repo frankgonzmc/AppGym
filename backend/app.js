9345c98a55ae593d3305981159e43f5e3b682d84
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-02 20:12:35 -0500

    update

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 import path from 'path';
-import multer from 'multer';
 import authRoutes from "./routes/auth.routes.js";
 import rutinaRoutes from "./routes/rutina.routes.js";
 import ejercicioRoutes from "./routes/ejercicio.routes.js";
@@ -27,7 +26,6 @@ app.use(cors({
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(multer());
 
 app.use("/api", authRoutes);
 app.use("/api", rutinaRoutes);
