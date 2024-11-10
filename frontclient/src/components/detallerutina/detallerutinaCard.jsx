b30f4a4d5e5988326eaea426ec15e13de1c12c4d
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-10 09:30:46 -0500

    update

--- frontclient/src/components/detallerutina/detallerutinaCard.jsx
+++ frontclient/src/components/detallerutina/detallerutinaCard.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useDetallesRutina } from "../../context/detallerutinacontext";
 import { Card } from "react-bootstrap"; // Asegúrate de tener react-bootstrap instalado
+import '../../css/detallePage.css';
 
 export default function DetalleRutinaCard({ detalles }) {
   const { deleteDetalleRutina } = useDetallesRutina();
@@ -29,24 +30,26 @@ export default function DetalleRutinaCard({ detalles }) {
   };
 
   return (
-    <Card className="text-white bg-primary">
-      <Card.Body>
-        <Card.Title className="text-center">{detalles.ejercicio.nombre}</Card.Title>
-        <Card.Subtitle className="mb-2 text-black text-center">Descripción: {detalles.ejercicio.descripcion}</Card.Subtitle>
-        <p className="text-center">Categoría: {detalles.ejercicio.categoria}</p>
-        <p className="text-center">Duración: {detalles.ejercicio.duracion} segundos</p>
-        <p className="text-center">Series: {detalles.ejercicio.series}</p>
-        <p className="text-center">Repeticiones: {detalles.ejercicio.repeticiones}</p>
-        <p className="text-center">Descanso: {detalles.ejercicio.descanso}</p>
-        <p className="text-center">Series Completadas: {detalles.seriesProgreso} / {detalles.ejercicio.series}</p>
-        <p className="text-center">Estado: {detalles.estado}</p>
-      </Card.Body>
-      <Card.Footer className="d-flex justify-content-between">
-        <button className="btn btn-danger" onClick={handleDelete}>Eliminar</button>
-        <button className="btn btn-success" onClick={handleStartOrContinueExercise}>
-          {detalles.seriesProgreso > 0 ? "Continuar Ejercicio" : "Iniciar Ejercicio"}
-        </button>
-      </Card.Footer>
-    </Card>
+    <section className="seccion-card">
+      <Card className="text-white bg-primary">
+        <Card.Body>
+          <Card.Title className="text-center">{detalles.ejercicio.nombre}</Card.Title>
+          <Card.Subtitle className="mb-2 text-black text-center">Descripción: {detalles.ejercicio.descripcion}</Card.Subtitle>
+          <p className="text-center">Categoría: {detalles.ejercicio.categoria}</p>
+          <p className="text-center">Duración: {detalles.ejercicio.duracion} segundos</p>
+          <p className="text-center">Series: {detalles.ejercicio.series}</p>
+          <p className="text-center">Repeticiones: {detalles.ejercicio.repeticiones}</p>
+          <p className="text-center">Descanso: {detalles.ejercicio.descanso}</p>
+          <p className="text-center">Series Completadas: {detalles.seriesProgreso} / {detalles.ejercicio.series}</p>
+          <p className="text-center">Estado: {detalles.estado}</p>
+        </Card.Body>
+        <Card.Footer className="d-flex justify-content-between">
+          <button className="btn btn-danger" onClick={handleDelete}>Eliminar</button>
+          <button className="btn btn-success" onClick={handleStartOrContinueExercise}>
+            {detalles.seriesProgreso > 0 ? "Continuar Ejercicio" : "Iniciar Ejercicio"}
+          </button>
+        </Card.Footer>
+      </Card>
+    </section>
   );
 }
