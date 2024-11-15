5f5cadc20a9a0746b320890fe4318e9a925b2703
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-15 18:39:24 -0500

    update

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,6 @@
 export const PORT = process.env.PORT || 5000;
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://database/rutinabd";
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "sxsecretxsx";
-
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "sxsecretxs";
+export const FRONTEND_URL = process.env.FRONTEND_URL
+  ? process.env.FRONTEND_URL.split(',')
+  : ["http://localhost:5173"];
