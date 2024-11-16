9006e3ce4c08bb3b74cc3bbf2d1cdb4ec341a485
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-16 12:30:47 -0500

    update

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({
-    origin: FRONTEND_URL.origin, // Usamos el array procesado
+    origin: FRONTEND_URL, // Usamos el array procesado
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
@@ -30,20 +30,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
-/*
-app.get("/api", (req, res) => {
-    res.json({
-        message: "Bienvenido a la API del GymApp",
-        availableRoutes: {
-            auth: "/api/auth",
-            rutina: "/api/rutinas",
-            ejercicio: "/api/ejercicios",
-            progreso: "/api/progreso",
-            detallerutina: "/api/detallerutina",
-            recomendacion: "/api/recomendaciones",
-        },
-    });
-});*/
 
 // Rutas de tu aplicación
 app.use("/api", authRoutes);
