2afbd48740bc0a2c90ec83a184ce70ccaea705b0
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-16 13:39:35 -0500

    update

--- frontclient/src/api/axios.js
+++ frontclient/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_APP_BACKEND_URL || 'http://100.125.85.115:5000/api',
+    baseURL: import.meta.env.VITE_APP_BACKEND_URL || 'http://localhost:5000/api',
     withCredentials: true,
 });
 export default instance;
