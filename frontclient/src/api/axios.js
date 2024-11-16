babb3c3e0f9a0f2c6d96173f0819d58178c3b016
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-15 19:54:05 -0500

    update

--- frontclient/src/api/axios.js
+++ frontclient/src/api/axios.js
@@ -1,8 +1,8 @@
-const baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL,
-    withCredentials: true
+    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api',
+    withCredentials: true,
 });
 
 export default instance;
