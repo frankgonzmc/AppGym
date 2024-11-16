9703cb736fde0bd9b29e777b46d3869ed28f0b4b
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-15 22:05:09 -0500

    update

--- backend/config.js
+++ backend/config.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = 5000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://database/rutinabd";
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "sxsecretxs";
+export const MONGODB_URI = "mongodb://database:27017/rutinabd";
+export const TOKEN_SECRET = "sxsecretxs";
 // Divide FRONTEND_URL en un array, o usa un valor por defecto
 export const FRONTEND_URL = process.env.FRONTEND_URL
   ? process.env.FRONTEND_URL.split(",")
