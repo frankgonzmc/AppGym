8cd5256b7851a5f7cd957f07f2753d7e30d0d945
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-17 08:48:24 -0500

    update

--- backend/controllers/rutina.controllers.js
+++ backend/controllers/rutina.controllers.js
@@ -6,12 +6,7 @@ import User from '../models/user.model.js'; // Importar modelo de usuario
 // Obtener todas las rutinas del usuario autenticado o predeterminadas
 export const getRutinas = async (req, res) => {
     try {
-        const rutinas = await Rutinas.find({
-            $or: [
-                { user: req.user.id },
-                { predeterminado: true } // Rutinas predeterminadas para todos los usuarios
-            ]
-        }).populate('user');
+        const rutinas = await Rutinas.find({ user: req.user.id }).populate('user');
         if (!rutinas || rutinas.length === 0) {
             return res.status(404).json({ message: "No se encontraron rutinas." });
         }
