e3676b28ced960a0793441f418fb5ae65fdddf56
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-24 10:41:43 -0500

    update

--- frontclient/src/pages/ejercicios/iniciarejercicioPage.jsx
+++ frontclient/src/pages/ejercicios/iniciarejercicioPage.jsx
@@ -24,6 +24,7 @@ export default function IniciaEjercicioPage() {
   const [duracionRestante, setDuracionRestante] = useState(detalles.ejercicio.duracion || 0);
   const [descansoRestante, setDescansoRestante] = useState(detalles.ejercicio.descanso || 0);
   const [seriesCompletadas, setSeriesCompletadas] = useState(detalles.seriesProgreso || 0);
+  const [caloriasQuemadas, setCaloriasQuemadas ] = useState(0);
   const [isPausado, setIsPausado] = useState(true);
   const [isDescanso, setIsDescanso] = useState(false);
   const [ejercicioCompletado, setEjercicioCompletado] = useState(false);
