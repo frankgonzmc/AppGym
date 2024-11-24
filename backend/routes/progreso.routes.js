4ce203fafd8529360d459ab878abd4185682c07d
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-24 01:56:33 -0500

    update

--- backend/routes/progreso.routes.js
+++ backend/routes/progreso.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { authRequired } from '../middlewares/validateToken.js';
 import { validateObjectId } from '../middlewares/validateId.js';
-import { createProgreso, deleteProgreso, getProgreso, updateProgreso, getUserStats, getUserStatsByPeriod, compareProgressWithGoals, registrarProgresoRutinaEjercicio } from '../controllers/progreso.controllers.js';
+import { createProgreso, deleteProgreso, getProgreso, updateProgreso, getUserStats, getUserStatsByPeriod, compareProgressWithGoals } from '../controllers/progreso.controllers.js';
 
 const router = Router();
 
@@ -15,7 +15,5 @@ router.delete('/progreso/:id', authRequired, validateObjectId, deleteProgreso);
 router.get('/stats/:userId', authRequired, getUserStats);
 router.get('/stats/:userId/:period', authRequired, getUserStatsByPeriod); // Cambiado POST a GET
 router.get('/compare-progress/:userId', authRequired, compareProgressWithGoals);
-
-router.post('/detalles-rutinas/:ejercicioId/registrar-completado', authRequired, registrarProgresoRutinaEjercicio);
 
 export default router;
