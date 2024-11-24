fd9821ba04620df1e4f46504a65db4db8b51d81a
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-24 10:58:35 -0500

    update

--- backend/models/progreso.model.js
+++ backend/models/progreso.model.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 const progresoSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     rutina: { type: mongoose.Schema.Types.ObjectId, ref: 'Rutina', required: true },
