90de5ee4b0cccadd7fc99c73dee22a8fd6920605
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-23 21:26:46 -0500

    update

--- frontclient/src/pages/auth/resetpasswordPage.jsx
+++ frontclient/src/pages/auth/resetpasswordPage.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from '../../api/axios';
+import '../../css/authPage.css';
 
 function ResetpasswordPage() {
     const { token } = useParams();
@@ -24,24 +25,26 @@ function ResetpasswordPage() {
     };
 
     return (
-        <div className="flex h-screen items-center justify-center">
-            <form onSubmit={handleSubmit(onSubmit)} className="bg-zinc-800 p-10 rounded-md">
-                <h2 className="text-white mb-4">Restablecer Contraseña</h2>
-                <input
-                    type="password"
-                    {...register('password', { required: true })}
-                    placeholder="Nueva Contraseña"
-                    required
-                    className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md mb-2"
-                />
-                {errors.password && (<p className="text-red-500">Se requiere la nueva contraseña</p>)}
+        <section className='seccion'>
+            <div className="flex h-screen items-center justify-center">
+                <form onSubmit={handleSubmit(onSubmit)} className="bg-zinc-800 p-10 rounded-md">
+                    <h2 className="text-white mb-4">Restablecer Contraseña</h2>
+                    <input
+                        type="password"
+                        {...register('password', { required: true })}
+                        placeholder="Nueva Contraseña"
+                        required
+                        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md mb-2"
+                    />
+                    {errors.password && (<p className="text-red-500">Se requiere la nueva contraseña</p>)}
 
-                <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md">
-                    {loading ? "Procesando..." : "Restablecer Contraseña"}
-                </button>
-                {message && <p className="text-red-500 mt-2">{message}</p>}
-            </form>
-        </div>
+                    <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md">
+                        {loading ? "Procesando..." : "Restablecer Contraseña"}
+                    </button>
+                    {message && <p className="text-red-500 mt-2">{message}</p>}
+                </form>
+            </div>
+        </section>
     );
 }
 
