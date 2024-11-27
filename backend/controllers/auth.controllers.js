775ffb2b7a5883410ee6c4f03cf1e29474847225
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-27 09:37:05 -0500

    update

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -241,7 +241,7 @@ export const updatePerfil = async (req, res) => {
 
 
         await user.save();
-        return res.status(200).json({ message: "Perfil actualizado correctamente", user });
+        return res.status(200).json(user);
     } catch (error) {
         console.error("Error en updatePerfil:", error);
         return res.status(500).json({ message: "Error al actualizar el perfil", error: error.message });
