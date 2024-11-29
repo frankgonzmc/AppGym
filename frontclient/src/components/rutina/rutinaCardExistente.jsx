1aef80c35fc2ee09ff0d8e0f3abaa78a30ca9d6e
ANGELES LARA JEAN POOL <73196982@continental.edu.pe>
2024-11-29 09:58:12 -0500

    update

--- frontclient/src/components/rutina/rutinaCardExistente.jsx
+++ frontclient/src/components/rutina/rutinaCardExistente.jsx
@@ -2,31 +2,32 @@ import React from "react";
 import { Card, Button } from "react-bootstrap";
 
 export function RutinaCardExistente({ rutina }) {
-  return (
-    <Card className="mb-4 text-black">
-      <Card.Header>
-        <h2 className="text-center">{`Rutina de ${rutina.categoria} (${rutina.nivel})`}</h2>
-      </Card.Header>
-      <Card.Body>
-        <ul>
-          {rutina.ejercicios.map((ejercicio, index) => (
-            <li key={index}>
-              <strong>{ejercicio.nombre}:</strong> {ejercicio.descripcion}.
-              <br />
-              <strong>Nivel: {ejercicio.nivel}.</strong>
-              <br />
-              <strong>series: {ejercicio.series} veces.</strong>
-              <br />
-              <strong>Duracion: {ejercicio.duracion} segundos.</strong>
-              <br />
-              <strong>Repeticiones: {ejercicio.repeticiones} veces.</strong>
-            </li>
-          ))}
-        </ul>
-      </Card.Body>
-      <Card.Footer className="text-center">
-        <Button variant="primary">Iniciar Rutina</Button>
-      </Card.Footer>
-    </Card>
-  );
+    return (
+        <Card className="mb-4 text-black">
+            <Card.Header>
+                <h2 className="text-center">{`Rutina de ${rutina.categoria} (${rutina.nivel})`}</h2>
+            </Card.Header>
+            <Card.Body>
+                <ul>
+                    {rutina.ejercicios.map((ejercicio, index) => (
+                        <li key={index}>
+                            <hr className="mb-4" />
+                            <strong>{ejercicio.nombre}:</strong> {ejercicio.descripcion}.
+                            <br />
+                            <strong>Nivel:</strong> {ejercicio.nivel}.
+                            <br />
+                            <strong>series:</strong> {ejercicio.series} veces.
+                            <br />
+                            <strong>Duracion:</strong>  {ejercicio.duracion} segundos.
+                            <br />
+                            <strong>Repeticiones:</strong>  {ejercicio.repeticiones} veces.
+                        </li>
+                    ))}
+                </ul>
+            </Card.Body>
+            <Card.Footer className="text-center">
+                <Button variant="primary">Iniciar Rutina</Button>
+            </Card.Footer>
+        </Card>
+    );
 }
